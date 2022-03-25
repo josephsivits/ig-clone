7802b0576d23ba3ee0233b8cbcdec04050ec0c77
Joseph Sivits <josephsivits@gmail.com>
2022-03-24 20:38:42 -0500

    finish stories component impl with dummy data

---
+++ components/Home/Stories.js
@@ -0,0 +1,47 @@
+import { View, Text, ScrollView,Image,StyleSheet } from 'react-native'
+import React from 'react'
+import {USERS} from '../../data/users'
+
+export
+
+const Stories = () => {
+  return (
+    <View style={{marginBottom:13}}>
+
+      <ScrollView 
+      horizontal 
+      showsHorizontalScrollIndicator={false}
+      >
+
+      {USERS.map((story,index) => (
+          <View key={index} style={{alignItems:'center'}}>
+            <Image source={{uri: story.image}} style={styles.story} />
+            <Text style={{color:'white'}}>{
+              story.user.length > 11 ? story.user.slice(0,10).toLowerCase() + '...' : story.user.toLowerCase()
+            }</Text>
+          </View>
+      ))}
+
+      </ScrollView>
+
+      <Text style={{color:'white'}}>STORIES</Text>
+
+    </View>
+  )
+}
+
+// DONE AT 46:52
+const styles = StyleSheet.create({
+    story: {
+      width: 70,
+      height: 70,
+      borderRadius:50,
+      //marginLeft: 6,
+      //marginRight: 6,
+      marginHorizontal:6,
+      borderWidth:3,
+      borderColor:'#FF8585',
+    }
+})
+
+export default Stories
