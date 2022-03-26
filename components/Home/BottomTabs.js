f2a5422e9a35d828ec2cc9baebd6a53f246fb9f4
Joseph Sivits <josephsivits@gmail.com>
2022-03-26 14:41:13 -0500

    added icons

--- components/Home/BottomTabs.js
+++ components/Home/BottomTabs.js
@@ -7,6 +7,31 @@ import {Divider} from 'react-native-elements'
 // Time to add the icons
 // active and unactive (filled and unfilled)
 export const bottomTabIcons = [
+    {
+        name:'Home',
+        active: 'https://img.icons8.com/fluency-systems-filled/144/ffffff/home',
+        inactive: 'https://img.icons8.com/fluency-systems-regular/144/ffffff/home',
+    },
+    {   
+        name:'Search',
+        active: 'https://img.icons8.com/ios-filled/344/ffffff/search--v1.png',
+        inactive: 'https://img.icons8.com/fluency-systems-regular/344/ffffff/search--v1.png',
+    },
+    {
+        name: 'Reels',
+        active: 'https://img.icons8.com/ios-filled/452/ffffff/instagram-reel.png',
+        inactive: 'https://img.icons8.com/ios/452/ffffff/instagram-reel.png',
+    },
+    {
+        name: 'Shop',
+        active: 'https://img.icons8.com/ios-glyphs/452/ffffff/shopping-bag-full.png',
+        inactive: 'https://img.icons8.com/windows/452/ffffff/shopping-bag-full.png',
+    },
+    {
+        name: 'Profile',
+        active: 'https://img.icons8.com/fluency-systems-filled/452/ffffff/user.png',
+        inactive: 'https://img.icons8.com/fluency-systems-regular/452/ffffff/user.png',
+    }
 
 ]
 
