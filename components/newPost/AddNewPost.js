487bd2ec13f8329c70713f7a9359ed2157df4314
Joseph Sivits <josephsivits@gmail.com>
2022-03-26 21:31:07 -0500

    Header for NewPost completed

--- components/newPost/AddNewPost.js
+++ components/newPost/AddNewPost.js
@@ -6,19 +6,34 @@ const AddNewPost = () => (
         <Header />
     </View>
 )
-
+/*
 const Header = () => (
     <View styles={styles.headerContainer}>
         <TouchableOpacity>
             <Image 
-                source={{uri: 'https://img.icons8.com/ios-glyphs/90/ffffff/back.png'}} 
+            
+                source={{uri: '../../assets/leftArrow.png'}} 
+                
                 style={{width:30,height:30}}
             />
-            
-      </TouchableOpacity>
-      <Text style={{color:'white'}}>ADD NEW POST</Text>
+            <Text>Ayo you a straightup bitch</Text>
+        </TouchableOpacity>
+      <Text style={styles.headerText}>NEW test POST bitch</Text>
+      <Text></Text>
     </View>
 )
+*/
+
+const Header = ({navigation}) => (
+    <View style={styles.headerContainer}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image source={require('../../assets/leftArrow.png')} style={{ width: 25, height: 25 }} />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>NEW POST</Text>
+        <View></View>
+      </View>
+  )
+
 
 const styles = StyleSheet.create({
     container: {
@@ -28,9 +43,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems:'center',
+        paddingBottom:15,
     },
     headerText: {
-
+        color: '#fff',
+        fontWeight: '700',
+        fontSize: 20,
+        marginRight:25,
     },
 
 })
