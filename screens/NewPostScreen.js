3fdfb2d579a6e9637fe0f75ce0157ac259406c68
Joseph Sivits <josephsivits@gmail.com>
2022-03-27 20:42:35 -0500

    safeareaview update to newpostscreen

--- screens/NewPostScreen.js
+++ screens/NewPostScreen.js
@@ -5,9 +5,10 @@ import AddNewPost from '../components/newPost/AddNewPost'
 
 const NewPostScreen = ({navigation}) => {
   return (
-    <SafeAreaView style={{ backgroundColor: 'black', flex: 1 }}>
+    <SafeAreaView style={[SafeAreaViewAndroid.AndroidSafeArea, styles.container]}> 
       <AddNewPost navigation={navigation} />
     </SafeAreaView>
+    
   )
 }
 
